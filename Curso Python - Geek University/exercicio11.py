6b4c6b58aec832e9dd403526c32bd6657ffc6357
Marcelo Bruno <sobm90@gmail.com>
2022-05-10 21:43:38 -0300

    Add exercicio11

---
+++ Curso Python - Geek University/exercicio11.py
@@ -0,0 +1,5 @@
+# Exercício 11
+velocidade = int(input('Digite uma velocidade em m/s: '))
+
+K = velocidade * 3.6
+print(f'A conversão da velocidade em m/s para Km/h é de: {K:.0f}km/h.')
--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# Repo com várias respostas
 Repositório com os exercícios de alguns cursos de lógica de programação e de python aplicados na linguagem python.
